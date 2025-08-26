60aea730afe29450de06da04a121e23b395d9bd8
yoshoku <yoshoku@outlook.com>
2025-08-27 08:38:33 +0900

    chore: fix output logic for updated test_set.csv

--- ml/main.py
+++ ml/main.py
@@ -33,18 +33,23 @@ def main():
   # Load and predict the test data
   diff_time = 0
   n_test_models = 0
+  results = {}
   with open('../dataset/test_set.csv', 'r') as file:
-    print('protein_id,class_id')
     reader = csv.DictReader(file)
     for row in reader:
-      protein_id = row['anonymised_protein_id'].replace('.vtk', '')
+      protein_id = row['protein_id'].replace('.vtk', '')
       start_time = time.time()
       descriptor = np.fromfile(f'../dataset/test_set/{protein_id}.dat', dtype=np.float32)
       class_id = pipeline.predict(descriptor.reshape(1, -1))
       end_time = time.time()
       diff_time += end_time - start_time
       n_test_models += 1
-      print(f'{protein_id},{class_id[0]}')
+      results[protein_id] = class_id[0]
+
+  sorted_results = {k: results[k] for k in sorted(results.keys(), key=int)}
+  print('protein_id,class_id')
+  for protein_id, class_id in sorted_results.items():
+    print(f'{protein_id},{class_id}')
 
   # Print the average time taken for prediction
   print(f'Average time taken for prediction: {diff_time / n_test_models} [s]', file=sys.stderr)
